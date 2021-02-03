a68840544ee2556f87f17fe7a6a209d8dfc0da11
Razvan <rabe@dhigroup.com>
2021-02-03 21:32:40 +0100

    add comm

--- src/story/CountingItem.tsx
+++ src/story/CountingItem.tsx
@@ -1,5 +1,5 @@
 import React, { FC, useState, useEffect } from 'react'
-import { Box, Typography, Grid } from '@material-ui/core'
+import { Box, Typography } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles'
 
 const useStyles = makeStyles(theme => ({
@@ -23,6 +23,7 @@ const CountingItem: FC<Props> = ({ max, name, duration }) => {
 
 		let start = 0
 
+		// eslint-disable-next-line radix
 		const end = parseInt(String(max).substring(0, 3))
 
 		if (start === end) return
